576c4a3aaabea9479e782ed727661e39f678b6e2
Alex Hasapis <alexmhasapis@gmail.com>
2015-04-02 22:33:47 -0400

    foundation

---
+++ db/migrate/20150402190202_raw_safety_report.rb
@@ -0,0 +1,22 @@
+class RawSafetyReport < ActiveRecord::Migration
+  def change
+    create_table :raw_safety_reports do |t|
+      t.string :building
+      t.string :dbn
+      t.integer :major_crimes
+      t.integer :other_crimes
+      t.integer :incidents
+      t.integer :property_crimes
+      t.integer :violent_crimes
+      t.integer :number_schools
+      t.string :group_size
+      t.decimal :avg_major_crimes, precision: 4, scale: 2
+      t.decimal :avg_other_crimes, precision: 4, scale: 2
+      t.decimal :avg_incidents, precision: 4, scale: 2
+      t.decimal :avg_property_crimes, precision: 4, scale: 2
+      t.decimal :avg_violent_crimes, precision: 4, scale: 2
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -73,15 +73,15 @@ puts Benchmark.measure{
 }
 
 puts Benchmark.measure{
-  sql.execute "TRUNCATE safety_reports"
+  sql.execute "TRUNCATE raw_safety_reports"
   sql.execute <<-SQL
   LOAD DATA INFILE "#{Rails.root.join('db','seeds','School_Safety_Report.csv')}"
-  INTO TABLE safety_reports
+  INTO TABLE raw_safety_reports
   FIELDS TERMINATED BY ','
   OPTIONALLY ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
-  (@id, building, dbn, @name, @code, @add, @boro, @geo, @reg, @bname, @schools, @inbldg, @major_crimes, @other_crimes, @incidents, @property_crimes, @violent_crimes, group_size, @range, @avg_major_crimes, @avg_other_crimes, @avg_incidents, @avg_property_crimes, @avg_violent_crimes)
+  (@id, building, dbn, @name, @code, @add, @boro, @geo, @reg, @bname, number_schools, @inbldg, @major_crimes, @other_crimes, @incidents, @property_crimes, @violent_crimes, group_size, @range, @avg_major_crimes, @avg_other_crimes, @avg_incidents, @avg_property_crimes, @avg_violent_crimes)
   SET
     major_crimes = REPLACE(@major_crimes, 'N/A', 0),
     other_crimes = REPLACE(@other_crimes, 'N/A', 0),
@@ -94,4 +94,15 @@ puts Benchmark.measure{
     avg_property_crimes = REPLACE(@avg_property_crimes, 'N/A', 0),
     avg_violent_crimes = REPLACE(@avg_violent_crimes, 'N/A', 0)
   SQL
+}
+
+puts Benchmark.measure{
+  sql.execute "TRUNCATE safety_reports"
+  sql.execute <<-SQL
+  INSERT INTO safety_reports (building, dbn, number_schools, major_crimes, other_crimes, incidents, property_crimes, violent_crimes, group_size, avg_major_crimes, avg_other_crimes, avg_incidents, avg_property_crimes, avg_violent_crimes, created_at, updated_at)
+  SELECT building, dbn, COUNT(number_schools) - 1, SUM(major_crimes) / number_schools, SUM(other_crimes) / number_schools, SUM(incidents) / number_schools, SUM(property_crimes) / number_schools, SUM(violent_crimes) / number_schools, group_size, avg_major_crimes / number_schools, avg_other_crimes / number_schools, avg_incidents / number_schools, avg_property_crimes / number_schools, avg_violent_crimes / number_schools, created_at, updated_at
+  FROM raw_safety_reports
+  GROUP BY building;
+  SQL
+  sql.execute "UPDATE safety_reports SET number_schools = 1 WHERE number_schools = 0;"
 }
